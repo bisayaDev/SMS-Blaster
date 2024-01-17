c33f5311f8e3ec050aef141846f89c5d88cace60
ryanMLqa <ryan@maddlogic.com>
2024-01-17 10:23:06 +0800

    fix allow multi blast

--- main.py
+++ main.py
@@ -14,13 +14,16 @@ app = FastAPI()
 
 
 async def background_task(duration: int, data: list):
-    print(f"Background task started: {duration} seconds")
+    print(f"Background task started: {duration} seconds interval")
 
     for item in data:
-        SendSms(item['text'],item['phone_number'],item['id'])
+        num = fix_cp_numbers(item['phone_number'])
+        if not num:
+            continue
+        SendSms(item['text'],num,item['id'],item['url'])
         await asyncio.sleep(duration)
 
-    print(f"Background task completed: {duration} seconds")
+    print(f"Background task completed: {duration * len(data)} seconds")
 
 async def bg_tasker(background_tasks: BackgroundTasks, data: list):
     background_tasks.add_task(background_task, 5, data)
@@ -34,13 +37,21 @@ async def root(base_url: str,job_id: int, background_tasks: BackgroundTasks = No
     else:
         return {"error":"Android SMS Gateway is offline."}
 
-def get_jobs(id,url):
-    location_url = f"https://{url}.ecitizenph.com/api"
+def get_jobs(id,base_url):
+    if base_url == "local":
+        url = "localhost"
+    else:
+        url = f"https://{base_url}.ecitizenph.com"
+
     try:
-        result = requests.get(f'{location_url}/sms/get-recipients/').json()
+        result = requests.get(f'{url}/api/sms/get-recipients/{id}').json()
     except Exception as e:
         result = []
         print(e)
+
+    if result:
+        for i in result:
+            i['url'] = url
     return result
 
 @app.get("/", response_class=HTMLResponse)
@@ -48,25 +59,29 @@ def index():
     return index_view()
 
 
-def SendSms(message,send_to,id):
+def SendSms(message,send_to,id,base_url):
   url = "http://" + os.getenv('APP_URL') + ":" + os.getenv('APP_PORT') + "/services/api/messaging/"
   params = {
     'to': send_to,
     'message': message
   }
   res = requests.post(url, params=params)
-  update_sent_msg_status("staging.ecitizenph.com/api",id)
+  update_sent_msg_status(base_url,id)
+
 def trim_data(data):
     json_data = json.loads(data)
     return json_data
 
 def update_sent_msg_status(base_url,id):
     try:
-        url = f'https://{base_url}/sms/{id}/update-sent/'
+        if base_url == 'local':
+            url = f'https://localhost/api/sms/{id}/update-sent/'
+        else:
+            url = f'{base_url}/api/sms/{id}/update-sent/'
         headers = {'Authorization': API_KEY}
         requests.put(url, headers=headers)
     except:
-        print("Database update error.")
+        print(f"Database update error. id = {id}")
 
 def ping_server_api():
     ping = Ping(os.getenv('APP_URL'),os.getenv('APP_PORT'),60)
@@ -76,3 +91,12 @@ def ping_server_api():
     except:
         return False
 
+def fix_cp_numbers(num):
+    if num.startswith('09') and len(num) == 11:
+        return num
+    elif num.startswith('+639') and len(num) == 13:
+        return num
+    elif num.startswith('639') and len(num) == 12:
+        return num
+    elif num.startswith('9') and len(num) == 10:
+        return f"0{num}"
