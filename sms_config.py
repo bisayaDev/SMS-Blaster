17ea5948b569be5d99689c294411a4f8703dd9d4
solomonben <solomon.johnbenedict@gmail.com>
2024-03-15 00:40:18 +0800

    fix username

--- sms_config.py
+++ sms_config.py
@@ -4,5 +4,5 @@ SMS_APP_PORT = 1688 #SMS Gateway Port from mymobkit
 NGROK_DOMAIN = 'open-joey-deadly.ngrok-free.app' #NGROK Domain
 BROADCAST_PORT = 9090 #Broadcast Port
 DISCORD_WEBHOOK = "https://discord.com/api/webhooks/1207168288736542720/czrxozLWQEcifpek9mnD2oNgtI5UJewSrOldh40cjC5edjusGNIuckoybHM2aLw_M-VW"
-TOKEN_USERNAME = "toke@ecitizenph.com"
+TOKEN_USERNAME = "token@ecitizenph.com"
 TOKEN_PASSWORD = "@dm1n098"
